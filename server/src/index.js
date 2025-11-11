befc159aaa07f29c4681aac61b0282da11a0a6cd
Dayo Bamgbelu <ddmds66@gmail.com>
2025-11-10 23:43:03 -0600

    vercel deployment

--- server/src/index.js
+++ server/src/index.js
@@ -8,8 +8,9 @@ dotenv.config();
 const app = express();
 
 // Configure CORS properly
+const corsOrigins = process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(',') : ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000'];
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000'],
+  origin: corsOrigins,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
